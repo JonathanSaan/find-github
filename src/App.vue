69efc332b96db31c5ef255a298771f9a9c3e07a6
Jonathan <crosspool03@gmail.com>
2023-11-12 16:25:04 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="container">
+    <Search />
+    <Profile />
+  </div>
+</template>
+
+<script>
+import Search from "./components/Search.vue";
+import Profile from "./components/Profile.vue";
+
+export default {
+  name: "App",
+  components: {
+    Search,
+    Profile,
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  font-family: Helvetica, Arial, sans-serif;
+  padding: 0;
+  margin: 0;
+}
+
+#container {
+  min-height: 100vh;
+  background-color: $background;
+
+  h1 {
+    color: $white;
+  }
+}
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Search profile github</h1>
+  <form></form>
+</template>
+
+<script>
+export default {
+  name: "Search",
+};
+</script>
+
+<style lang="scss" scoped></style>
